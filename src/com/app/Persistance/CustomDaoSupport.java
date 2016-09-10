f65ab87dd5472a94c29dc8161e10577f2761a8b0
namannigam <naman.nigam@flipkart.com>
2016-09-11 03:33:32 +0530

    Moving off some burden

--- src/com/app/Persistance/CustomDaoSupport.java
+++ src/com/app/Persistance/CustomDaoSupport.java
@@ -1,9 +1,8 @@
-package com.app.Persistance;
+package com.app.persistance;
 
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate4.support.HibernateDaoSupport;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 @Service
 public abstract class CustomDaoSupport extends HibernateDaoSupport {
